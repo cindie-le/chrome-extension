df642659d13366abaca395ecae1cac4a2b411b78
cindie-le <cvle104@gmail.com>
2024-01-02 18:29:35 -0800

    Created an input field and save button. Any input text saved will be displayed in a list below.

---
+++ src/index.js
@@ -0,0 +1,19 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+
+inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value)
+    renderLeads()
+})
+
+const renderLeads = () => {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>"
+    }
+    //Render listITems inside the unordered list
+    ulEl.innerHTML = listItems
+}
