3b3a9e4e0f8eb38bacf2d6d2d35af45ea503b783
cindie-le <cvle104@gmail.com>
2024-01-03 21:46:49 -0800

    Save links locally in the browser when the button is clicked

--- bin/index.js
+++ bin/index.js
@@ -3,17 +3,26 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-
-inputBtn.addEventListener("click", function () {
+localStorage.clear()
+let leadsFromLocalStorage =  JSON.parse(localStorage.getItem("myLeads"))
+inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
+    
 })
 
-const renderLeads = () => {
+function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>"
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
     }
-    //Render listITems inside the unordered list
-    ulEl.innerHTML = listItems
+    ulEl.innerHTML = listItems  
 }
